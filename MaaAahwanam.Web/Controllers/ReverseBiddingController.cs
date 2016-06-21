605955265ffba0542a70380bacfbf3833cd0108b
sai krishna reddy <saikrishnareddythummala@gmail.com>
2016-06-21 10:55:27 +0530

    21st june

---
+++ MaaAahwanam.Web/Controllers/ReverseBiddingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class ReverseBiddingController : Controller
+    {
+        //
+        // GET: /ReverseBidding/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ServiceRequest serviceRequest)
+        {
+            ServiceRequestService serviceRequestService = new ServiceRequestService();
+            serviceRequestService.SaveService(serviceRequest);
+            return View();
+        }
+	}
+}
