5d197275fab5444eea1e6fe6a1c5f01f39aaeea7
sai krishna reddy <saikrishnareddythummala@gmail.com>
2016-07-28 17:44:11 +0530

    Changes on 28th july 16

---
+++ MaaAahwanam.Web/Controllers/VendorBiddingslistController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MaaAahwanam.Service;
+using MaaAahwanam.Models;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class VendorBiddingslistController : Controller
+    {
+        // GET: VendorBiddingslist
+        public ActionResult Index(ServiceRequest serviceRequest)
+        {
+            serviceRequest.Type = "Bidding";
+            ServiceRequestService serviceRequestService = new ServiceRequestService();
+            List<ServiceRequest> bidlist=serviceRequestService.GetServiceRequestList(serviceRequest);
+            ViewBag.bidlist = bidlist;
+            return View();
+        }
+    }
+}
